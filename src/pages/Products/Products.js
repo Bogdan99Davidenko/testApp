dc46dcc35ac743beb58952e995fc3e738e6ee16b
Bogdan99Davidenko <33360343+Bogdan99Davidenko@users.noreply.github.com>
2020-05-10 00:53:32 +0300

    add registration folders

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Products.scss";
+import axios from "axios";
 
 function Products() {
+  async function getCards() {
+    const data = await axios.get(
+      "https://testapp-4fc4f.firebaseio.com/cards.json"
+    );
+    console.log(data);
+  }
+  getCards();
   return (
     <div className="container">
       <div className="header">
